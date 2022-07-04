35efff90700c2c381b6b1b37f06cd40650beaf25
LucasLima <lugablima@gmail.com>
2022-07-04 10:22:16 -0300

    chore: adjust environment variables for deploy on heroku

--- src/db.js
+++ src/db.js
@@ -6,6 +6,6 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
 
-const db = mongoClient.db(process.env.MONGO_DB_NAME);
+const db = mongoClient.db(process.env.MONGO_DATABASE);
 
 export default db;
